d1b89496dcdf87fea05e88e2c028b2c9e4013842
Maxx <73094553+Lochagos1@users.noreply.github.com>
2022-05-23 13:43:27 +0800

    New Code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const isLocalhost = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
 const STORAGE_API_HOST = isLocalhost ? `http://localhost:3000` : `https://keyval-store.herokuapp.com`;
-
 const letterGrades = {
     4: 'A',
     3.5: 'B+',
@@ -13,6 +12,27 @@ const letterGrades = {
     0: 'F',
 };
 const modules = {};
+
+function setShareLinkInInputField(inputField, key) {
+    const url = new URL(window.location.href);
+    url.searchParams.set('key', key);
+    inputField.value = url.toString();
+}
+
+function deleteComplete() {
+    // empty function
+    // Call this function after you're sure that deletion is completed
+}
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const charactersLength = characters.length;
+function makeId(length) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 window.addEventListener('DOMContentLoaded', function () {
     // Table
@@ -30,12 +50,14 @@ window.addEventListener('DOMContentLoaded', function () {
     const gradeInput = addModuleForm.querySelector('#grade');
 
     // Generate sharing link
-     //const generateLinkButton = document.querySelector('#generate-link');
-    const setKeyButton = document.querySelector('#set-key');
-    const shareLinkInput = document.querySelector('#user-key');
+    const generateLinkButton = document.querySelector('#generate-link');
+    const shareLinkInput = document.querySelector('#share-link');
     const timeGeneratedField = document.querySelector('#time-generated');
-    const X = document.querySelector('#user-key');
-    //const X = addUserKey.querySelector('#user-key');
+
+    // Get reference to custom generate controls
+    const customGenerateLinkButton = document.querySelector('#custom-generate-link'); // isaac
+    const customShareLinkResult = document.querySelector('#custom-share-link'); // kylie
+    const customShareLinkInput = document.querySelector('#custom-share-link-input'); // kylie
 
     // All intractable controls (e.g. input, buttons, etc...)
     const controls = [
@@ -43,7 +65,7 @@ window.addEventListener('DOMContentLoaded', function () {
         moduleNameInput,
         creditInput,
         gradeInput,
-        setKeyButton,
+        generateLinkButton,
     ];
 
     /**
@@ -76,7 +98,7 @@ window.addEventListener('DOMContentLoaded', function () {
      * Create a new row and update modules object
      */
     function createModuleWithId(moduleName, credit, grade) {
-        const id = Date.now();
+        const id = makeId(10);
         modules[id] = { name: moduleName, credit, grade };
         const newRow = createRow(moduleName, credit, grade, (newRow) => {
             moduleTableBody.removeChild(newRow);
@@ -144,12 +166,11 @@ window.addEventListener('DOMContentLoaded', function () {
     /**
      * Uploads modules data to storage and generate sharing link based on returned key
      */
-  //generateLinkButton.onclick = function () {
-    setKeyButton.onclick = function () {
+    generateLinkButton.onclick = function (e) {
+        e.preventDefault();
         disablePage();
         const modules = getModules();
-        const userKey = X.value;
-        fetch(`${STORAGE_API_HOST}/storage?key=${userKey}` , {
+        fetch(`${STORAGE_API_HOST}/storage`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -158,14 +179,34 @@ window.addEventListener('DOMContentLoaded', function () {
         })
             .then((response) => response.json())
             .then((json) => {
-                const key = json.key;
-                const url = new URL(window.location.href);
-                url.searchParams.set('key', key);
-                shareLinkInput.value = url.toString();
+                setShareLinkInInputField(shareLinkInput, json.key);
                 timeGeneratedField.textContent = new Date().toLocaleString();
             })
             .catch((error) => alert(error.message))
             .finally(() => enablePage());
+    };
+
+    //Listen to the click event of customGenerateLinkButton (for now, on click alert)
+    customGenerateLinkButton.onclick = function (e) {
+        e.preventDefault();
+        const inputValue = customShareLinkInput.value;
+        fetch(`${STORAGE_API_HOST}/storage?key=${inputValue}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(modules),
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                const key = json.key;
+                // assign the key to the value of the input field customShareLinkResult
+                const url = new URL(window.location.href);
+                url.searchParams.set('key', key);
+                customShareLinkResult.value = url.toString();
+            });
     };
 
     /**
@@ -186,6 +227,14 @@ window.addEventListener('DOMContentLoaded', function () {
             .catch((error) => alert(error.message))
             .finally(() => enablePage());
     }
+
+    // Get a reference to the button
+    const deleteButton = document.querySelector('#delete-button');
+    deleteButton.onclick = function (e) {
+        fetch(`${STORAGE_API_HOST}/storage`, { method: 'DELETE' });
+        .then((response)) => alert(response.message)
+    };
+
 
     /**
      * Check for key in url and loads module data
